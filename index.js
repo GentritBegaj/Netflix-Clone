7b30aae78512898f519c8bb5864721bc92ed9557
GentritBegaj <gentritbegaj@gmail.com>
2021-03-21 20:21:30 +0100

    edit button added

--- index.js
+++ index.js
@@ -34,45 +34,21 @@ window.onload = async () => {
   const data = await response.json();
 
   console.log(data);
-  let count = 0;
-  data.forEach((category, idx) => {
-    let mainDiv = document.getElementById("main-div");
-    mainDiv.innerHTML += `<div class="category-heading"><h4 class="text-light text-left">${category}</h4></div>
-        <div
-          id="carouselExampleControls${idx}"
-          class="carousel slide px-0  text-center"
-          data-ride="carousel"
-          data-interval="false"
-        >
 
-          <div class="carousel-inner">
-            <div class="carousel-item active">
-              <div id="main-row${idx}" class="row no-gutters d-flex flex-nowrap"></div>
-            </div>
-            <div  class="carousel-item">
-            <div id="second-row${idx}" class="row no-gutters d-flex flex-nowrap"></div>
-            </div>
-          </div>
-          <a
-            class="carousel-control-prev"
-            href="#carouselExampleControls${idx}"
-            role="button"
-            data-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a
-            class="carousel-control-next"
-            href="#carouselExampleControls${idx}"
-            role="button"
-            data-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>`;
-    const response1 = fetch(`${url}/${category}`, {
+  let mainDiv = document.getElementById("main-div");
+  data.forEach((category, index) => {
+    mainDiv.innerHTML += `<div>
+                            <div class="w-100 px-5"><h4 class="text-light text-left">${category}</h4></div>
+                                                        <div id="image-slider-${index}" class="splide mb-5">
+                                <div class="splide__track">
+                                    <ul class="splide__list list__${index}">
+                                    </ul>
+                                </div>
+                            </div>
+                            
+                            </div>`;
+
+    fetch(`${url}/${category}`, {
       headers: {
         Authorization:
           "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MDU0N2I4NGVjNWEzNTAwMTU0MGQ2ZGYiLCJpYXQiOjE2MTYxNDkzODEsImV4cCI6MTYxNzM1ODk4MX0.A6mOfd_ng0V_z_rWBgKlV2ZDFYuNtNeA1Yt47EYvveo",
@@ -81,19 +57,30 @@ window.onload = async () => {
       .then((response1) => response1.json())
       .then((data1) => {
         console.log(data1);
-        let mainRow = document.getElementById(`main-row${idx}`);
-        let secondRow = document.getElementById(`second-row${idx}`);
-        data1.forEach((movie, idx1) => {
-          if (idx1 < 6) {
-            mainRow.innerHTML += `<div class="col-6 col-sm-4 col-md-3 col-lg-2 mb-4">
-                            <a href="backoffice.html?id=${movie._id}&category=${movie.category}"><img src=${movie.imageUrl} alt="movie-photo" class="img-fluid" /></a>
+        let carouselList = document.querySelector(`.list__${index}`);
 
-                                  </div>`;
-          } else {
-            secondRow.innerHTML += `<div class="col-12 col-sm-6 col-md-4 col-lg-2 mb-4">
-                                    <<a href="backoffice.html?id=${movie._id}&category=${movie.category}"><img src=${movie.imageUrl} alt="movie-photo" class="img-fluid" /></a>
-                                  </div>`;
-          }
+        data1.forEach((movie) => {
+          carouselList.innerHTML += ` <li class="splide__slide"><img src="${movie.imageUrl}"></li>`;
+
+          new Splide(`#image-slider-${index}`, {
+            perPage: 6,
+            pagination: false,
+            drag: true,
+            speed: 800,
+            easing: "cubic-bezier(.42,.65,.27,.99)",
+            perMove: 1,
+            breakpoints: {
+              800: {
+                perPage: 4,
+              },
+              640: {
+                perPage: 2,
+              },
+              480: {
+                perPage: 1,
+              },
+            },
+          }).mount();
         });
       })
       .catch((err) => console.log(err));
